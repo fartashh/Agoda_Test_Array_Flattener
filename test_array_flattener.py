fbdbc0a3412ee92d622e7384151386707e2626f2
Fartash <fartash@mindvalley.com>
2016-11-03 11:45:53 +0800

    add test for 3 level nested

--- test_array_flattener.py
+++ test_array_flattener.py
@@ -12,6 +12,14 @@ class ArrayFlattenerTestCase(unittest.TestCase):
         result = array_flattner.get_result()
         self.assertListEqual(result, expected_result)
 
+    def test_3_level_nested_array(self):
+        sample = [4,[3,2,[9,0]],[1,2,3,[8,7]]]
+        expected_result =[4,3,2,9,0,1,2,3,8,7]
+        array_flattner = ArrayFlattener(sample)
+        array_flattner.process()
+        result = array_flattner.get_result()
+        self.assertListEqual(result, expected_result)
+
 
 if __name__ == "__main__":
     unittest.main()
