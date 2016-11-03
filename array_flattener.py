1ede2e881f4ec71823967531a9c759369fe383e0
Fartash <fartash@mindvalley.com>
2016-11-03 11:51:47 +0800

    flattener logic

---
+++ array_flattener.py
@@ -0,0 +1,36 @@
+import json
+
+
+class ArrayFlattener(object):
+    def __init__(self, nested_array):
+        """
+        Instantiate with nested array, call .process and then .get_result()
+        :param nested_array:
+        :return: flat array
+        """
+        self.flat_array = []
+        self.nested_array = nested_array
+
+    def process(self):
+        self.flat_array = list(self.__flattener(self.nested_array))
+        return self
+
+    def get_result(self, result_type='list'):
+        """
+        get results in your preferred type
+        :param result_type: define the expected out put type 'list','json' by default it is obj
+        :return:
+        """
+        result_factory = {
+            'json': json.dumps(self.flat_array),
+            'list': self.flat_array,
+        }
+        return result_factory[result_type]
+
+    def __flattener(self, list):
+        for elm in list:
+            try:
+                for _elm in self.__flattener(elm):
+                    yield _elm
+            except:
+                yield elm
