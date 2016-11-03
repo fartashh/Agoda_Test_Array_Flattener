143e41480700a4e5301d4f1094151dcb4e30b327
Fartash <fartash@mindvalley.com>
2016-11-03 11:46:06 +0800

    add test for n level nested

--- test_array_flattener.py
+++ test_array_flattener.py
@@ -20,6 +20,13 @@ class ArrayFlattenerTestCase(unittest.TestCase):
         result = array_flattner.get_result()
         self.assertListEqual(result, expected_result)
 
+    def test_n_level_nested_array(self):
+        sample = [4,[3,2,[9,0,[1,2]]],[1,[5,6,7],2,3,[8,7]],[9,8,[1,[4,5,[7]]]]]
+        expected_result =[4,3,2,9,0,1,2,1,5,6,7,2,3,8,7,9,8,1,4,5,7]
+        array_flattner = ArrayFlattener(sample)
+        array_flattner.process()
+        result = array_flattner.get_result()
+        self.assertListEqual(result, expected_result)
 
 if __name__ == "__main__":
     unittest.main()
